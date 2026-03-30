4fbb23ec861c628c8a32ff2c47ab4966b3167aa5
karrrraphael-creator <karrrraphael@gmail.com>
2026-03-30 18:23:39 +0200

    Create Login.java

---
+++ Login.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Login {
+    public static void main(String[] args) {
+        JFrame loginFrame = new JFrame("Authentification");
+        loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        loginFrame.setSize(300,150);
+        loginFrame.setLayout(new GridLayout(3,2,5,5));
+
+        JLabel userLabel = new JLabel("Utilisateur :");
+        JTextField userField = new JTextField();
+
+        JLabel passLabel = new JLabel("Mot de passe :");
+        JPasswordField passField = new JPasswordField();
+
+        JButton loginButton = new JButton("Se connecter");
+
+        loginFrame.add(userLabel);
+        loginFrame.add(userField);
+        loginFrame.add(passLabel);
+        loginFrame.add(passField);
+        loginFrame.add(new JLabel()); 
+        loginFrame.add(loginButton);
+
+        loginButton.addActionListener(e -> {
+            String user = userField.getText();
+            String pass = new String(passField.getPassword());
+
+            if(user.equals("admin") && pass.equals("1234")) {
+                loginFrame.dispose();
+                Interface.main(null);
+            } else {
+                JOptionPane.showMessageDialog(loginFrame,
+                        "Utilisateur ou mot de passe incorrect !",
+                        "Erreur",
+                        JOptionPane.ERROR_MESSAGE);
+            }
+        });
+
+        loginFrame.getRootPane().setDefaultButton(loginButton);
+
+        loginFrame.setLocationRelativeTo(null);
+        loginFrame.setVisible(true);
+    }
+}
