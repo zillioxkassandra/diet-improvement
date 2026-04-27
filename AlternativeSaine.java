359906395cc082f203df4d50c2bc1b53d0cf779e
Kassandra <zilliox.kassandra@gmail.com>
2026-04-27 16:44:52 +0200

    slight changes to Alternative Saine for future coding

--- AlternativeSaine.java
+++ AlternativeSaine.java
@@ -1,7 +1,16 @@
-public class Alternative_saine {
-    Consommable alternative_saine;
-    public void alt(ingredient, qte) {};
+public class AlternativeSaine {
+
+    public Consommable alternative_saine;
+    public ArrayList<Repas> repas;
+
+    public void alt(Ingredient ingredient, float qte) {
+        // Compute nutri values and find the worst
+        //from bdd in same food category : 
+            // find better than the worst
+        //set alternative_saine to better
+    };
     public void AfficherInfoAlt() {
-        Utilisateur.afficherInfos();
+        // Ask bdd for nutri values
+        // print infos
     };
 }
